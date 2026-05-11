274dfa43ddc51b1c0093c5066b19d4dee3c9fd71
EmilyUM <dc32635@um.edu.mo>
2026-05-12 03:44:23 +0800

    Initial commit (SugEx09)

---
+++ public/cisc3003-sugex09.php
@@ -0,0 +1,74 @@
+<?php
+$genres = ["Abstract", "Baroque", "Gothic", "Renaissance"];
+$subjects = ["Animals", "Landscape", "People"];
+
+function generateOptions($values) {
+    $html = "";
+    foreach ($values as $value) {
+        $escaped = htmlspecialchars($value, ENT_QUOTES, "UTF-8");
+        $html .= "<option value=\"$escaped\">$escaped</option>";
+    }
+    return $html;
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>CISC3003 Suggested Exercise 09</title>    
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+    <script src="js/misc.js"></script>
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+
+<?php include 'header.inc.php'; ?>
+    
+<main>
+<form class="form" id="mainForm" method="post" action="art-process.php">
+   <fieldset class="form__panel">
+      <legend class="form__heading">Edit Art Work Details</legend>
+        <p class="form__row">
+           <label>Title</label><br>
+           <input type="text" name="title" class="form__input form__input--large">
+       </p>
+       <p class="form__row">
+           <label>Description</label><br>
+           <input type="text" name="description" class="form__input form__input--large">
+       </p>            
+       <p class="form__row"> 
+           <label>Genre</label><br>
+           <select name="genre" class="form__input form__select">
+              <option value="">Choose genre</option> 
+              <?php echo generateOptions($genres); ?>
+           </select>
+       </p>
+       <p class="form__row"> 
+           <label>Subject</label><br>
+           <select name="subject" class="form__input form__select">
+              <option value="">Choose subject</option> 
+              <?php echo generateOptions($subjects); ?>
+           </select>
+       </p>
+       <p class="form__row">	
+           <label>Medium</label><br>               
+           <input type="text" name="medium" class="form__input form__input--medium">
+       </p>
+       <p class="form__row">	
+           <label>Year</label><br>               
+           <input type="text" name="year" class="form__input form__input--small">
+       </p>  
+       <p class="form__row">	
+           <label>Museum</label><br>               
+           <input type="text" name="museum" class="form__input form__input--medium">
+       </p>                             
+
+       <div class="form__box"> 
+          <input type="submit" value="Submit" class="form__btn"> <input type="reset" value="Clear Form" class="form__btn">      
+       </div>
+   </fieldset>
+</form>
+</main>       
+</body>
+</html>
